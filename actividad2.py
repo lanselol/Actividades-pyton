eb245c24570fe869488598a6a380c75bee56010b
eduardo dominguez <zancheze559@gmail.com>
2024-11-23 14:07:08 -0600

    Agregue la actividad2.py

---
+++ actividad2.py
@@ -0,0 +1,28 @@
+'''
+num = int(input("Ingresa el número:"))
+if num > 0 :
+    print("Numero positivo")
+else:
+    print("Numero negativo")
+num= int(input("Ingresa tu numero"))
+if num % 2==0:
+    print("El numero es par")
+else:
+    print("El numero es inpar")
+edad = int(input("Ingresa tu edad:"))
+if edad >18 :
+    print("Mayor que 18")
+else:
+    print("Menor que 18")    
+num1= int(input("Ingresa el numero:"))
+num2 = int(input("Ingresa el segundo numero:"))
+if num1>num2:
+    print("El primero es el mayor ")
+else:
+    print("El segundo es el mayor")
+num=int(input("Ingresa tu radio: "))
+Area=3.1416*(num*num)
+print("El area del circulo es:",Area)
+    '''
+num=int(input("Numero solicitado:"))
+rango=10
