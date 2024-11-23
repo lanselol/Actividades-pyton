56ba7126e1cc85caa4ce8a4bae9d6beff7dc21a2
eduardo dominguez <zancheze559@gmail.com>
2024-11-23 13:56:33 -0600

    Agregue el actividad.py

---
+++ actividad.py
@@ -0,0 +1,59 @@
+age=int(input("edad del alunmo:"))
+if age == 23 or age == 25:
+    print("true")
+else:
+    print("false")
+
+dinero=int(input("dinero a ocupar: "))
+if dinero>=300 or dinero <=600:
+    print("true")
+else:
+    print("false")
+
+puntos=int(input("puntos conseguidos: "))
+if puntos>=3000 or puntos <=5000:
+    print("true")
+else:
+    print("false")
+
+copas=int(input("copas ganadas: "))
+if copas<=200 or copas >=500 :
+    print("true")
+else:
+    print("false")
+
+elexir=int(input("elixir requerido:"))
+if elexir>=5 or elexir ==10:
+    print("true")
+else:
+    print("false")
+
+juegos=int(input("juegos comprados: "))
+if juegos<=11 or juegos >=22:
+    print("true")
+else:
+    print("false")
+
+computadoras=int(input("ingresa las computadoras: "))
+if computadoras>=28 or computadoras ==35:
+    print("true")
+else:
+    print("false")
+
+platillos= int(input("ingresa los platillos: "))
+if platillos<=12 or platillos >=20:
+    print("true")
+else:
+    print("false")
+
+mesas= int(input("cuantas mesas: "))
+if mesas>=52 or mesas ==60:
+    print("true")
+else:
+    print("false")
+
+tvs=int(input("ingresa las tvs: "))
+if tvs==12 or tvs <=13:
+    print("true")
+else:
+    print("false")
